d2bc24b53894862bbd833861b54aff32d71af3ac
pmpr.dev <pmpr.dev@gmail.com>
2024-09-14 19:45:31 +0000

    Obfuscated

---
+++ build/js/register.275e_f43y.js
@@ -0,0 +1,2 @@
+/*! For license information please see register.275e_f43y.js.LICENSE.txt */
+PMPRInlineUtil.registerListener("load",(function(e){"serviceWorker"in navigator?navigator.serviceWorker.getRegistrations().then((function(e){for(let r of e)r.unregister()})):console.log("ServiceWorker not exist.")}));
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             66e5e7b649eed             |
+    |_______________________________________|
+*/
+ use Pmpr\Module\PWA\PWA; PWA::symcgieuakksimmu();
